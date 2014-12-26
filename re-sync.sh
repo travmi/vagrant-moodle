269c3c059fccf69fc6e3fe383946aedbbd4811fe
Mike Travis <mike.r.travis@gmail.com>
2014-12-26 15:17:04 -0600

    Added documentation to the re-sync script.

--- re-sync.sh
+++ re-sync.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # This script syncs your moodle directory to /var/www so you can perform development testing.
+# Rsync source is a folder placed in the root of the project and then rsync over to /var/www.
 
 rsync -drvlp --delete --exclude config.php /vagrant/moodle/src/* /var/www/local/moodle.local.lan/htdocs/
 chown -R apache:apache /var/www/local
